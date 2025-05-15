98bae1d7aa860620601c94a64d091ef681629e82
Joy Wang <108701016+joyqvq@users.noreply.github.com>
2025-05-15 15:45:39 -0400

    use enclave object id

--- frontend/src/networkConfig.ts
+++ frontend/src/networkConfig.ts
@@ -5,7 +5,7 @@ import { createNetworkConfig } from "@mysten/dapp-kit";
 
 const EXAMPLE_PACKAGE_ID = "0xffe4466f1b98b2834327eb0757823323abed4756d0fe4ddd240dfc91abe3b39d";
 const ENCLAVE_CONFIG_OBJECT_ID = "0x4d933a25438f2bf9b59cd2c16e81e30b41a7ee0f2a3172396952ce1d7676bfc2";
-const ENCLAVE_OBJECT_ID = "0x62afa41b677c59037ba4e893367e86e56cb9bdbfa987fe0e835cdce474b54ece";
+const ENCLAVE_OBJECT_ID = "0x5ea48cc2883aaf4fa040293dab5fd033dbe64f61d70630b077c311503766be02";
 
 const { networkConfig, useNetworkVariable, useNetworkVariables } =
   createNetworkConfig({
